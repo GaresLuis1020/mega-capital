a22306d0f06fe83a5db9144119b41b681332bb0e
Gares Luis <46531267+GaresLuis1020@users.noreply.github.com>
2022-05-06 21:34:40 +0800

    Update design.js

--- src/redux/slices/design.js
+++ src/redux/slices/design.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-// utils
+// utils 
 import axios from "../../utils/axios";
 // ----------------------------------------------------------------------
 
