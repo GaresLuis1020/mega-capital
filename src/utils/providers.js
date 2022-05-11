c04e601280ecfc74a09c0362ead100e73bef5bb7
Gares Luis <46531267+GaresLuis1020@users.noreply.github.com>
2022-05-11 08:17:14 +0800

    Update providers.js

--- src/utils/providers.js
+++ src/utils/providers.js
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import getRpcUrl from "./getRpcUrl";
+import getRpcUrl from "./getRpcUrl"; 
 
 export const simpleRpcProvider = (chainID) =>
   new ethers.providers.StaticJsonRpcProvider(getRpcUrl(chainID));
