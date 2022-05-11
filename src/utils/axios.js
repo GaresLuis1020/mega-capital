873a43682d3e9ac1f50c7f3da77d75d75fa21674
Gares Luis <46531267+GaresLuis1020@users.noreply.github.com>
2022-05-11 08:11:45 +0800

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
  
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
-});
+}); 
 
 axiosInstance.interceptors.response.use(
   (response) => response,
